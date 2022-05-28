52ffa4a661f62623529cf3e2965f78d00732338f
juan <jjose230391@gmail.com>
2022-05-28 16:14:10 -0300

    Update security in endpoint, verify with JWT

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -1,17 +1,15 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
-const db = require("../models");
-const User = db.user;
 
 verifyToken = (req, res, next) => {
-    let token = req.headers["x-access-token"];
+    let token = req.headers["authorization"];
 
     if (!token) {
         return res.status(403).send({
             message: "No token provided!"
         });
     }
-
+    token = token.split(' ').length ? token.split(' ')[1] : token;
     jwt.verify(token, config.secret, (err, decoded) => {
         if (err) {
             return res.status(401).send({
--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -2,24 +2,25 @@
 module.exports = app => {
 
   const contacts = require("../controllers/contacts.js");
+  const { authJwt } = require("../middleware");
 
   var router = require("express").Router();
 
   // Create a new Contact
-  router.post("/", contacts.create);
+  router.post("/", [authJwt.verifyToken],contacts.create);
 
   // Retrieve all Contact
-  router.get("/", contacts.findAll);
+  router.get("/", [authJwt.verifyToken], contacts.findAll);
 
 
   // Retrieve a single Contacts with id
-  router.get("/:id", contacts.findOne);
+  router.get("/:id", [authJwt.verifyToken], contacts.findOne);
 
   // Update a Contact with id
-  router.put("/:id", contacts.update);
+  router.put("/:id", [authJwt.verifyToken], contacts.update);
 
   // Delete a Contact with id
-  router.delete("/:id", contacts.delete);
+  router.delete("/:id", [authJwt.verifyToken], contacts.delete);
 
   app.use('/api/contacts', router);
 };
